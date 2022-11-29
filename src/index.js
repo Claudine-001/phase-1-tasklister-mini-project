4228b1ac4d45444b1e5e26a861f9a330f45ce956
claudine-001 <claudine.kimani@student.moringaschool.com>
2022-11-30 00:47:55 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+let form = document.querySelector('form')
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  buildToDos(e.target) 
+  form.reset()
+}) 
+})
+function buildToDos(form){
+  let toDo = form.new_task_description.value
+  let toDos = document.getElementById('tasks')
+  let li =document.createElement('li')
+  li.textContent = toDo
+  toDos.appendChild(li)
+}
